a30b2b5293da629deda284b807fd984a86ef56e1
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2026-03-05 18:42:07 +0300

    added phone and joined date to models

--- members/models.py
+++ members/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Member(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length= 100)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+    
