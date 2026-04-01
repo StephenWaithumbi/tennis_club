30009e1ff6254e22132cf4dfef55d4ec35fa2a96
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2026-04-01 20:07:39 +0300

    fools day

--- members/models.py
+++ members/models.py
@@ -7,3 +7,4 @@ class Member(models.Model):
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
     
+    
--- members/views.py
+++ members/views.py
@@ -18,7 +18,14 @@ def details(request, id):
     mymember = Member.objects.get(id=id)
     template = loader.get_template('details.html')
     context = {
-        'mymember': mymember
+        'mymember': mymember 
+        
     }
-    return HttpResponse(template,render(context, request))
+    return HttpResponse(template.render(context, request))
 
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
